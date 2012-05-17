b0a2dbc85732aa0193a5b96ab4ffee90bf1a7340
wang-bin <wbsecg1@gmail.com>
2012-05-17 22:46:02 +0800

    more test

--- test/tst_options.cpp
+++ test/tst_options.cpp
@@ -8,22 +8,29 @@ int main(int argc, char** argv)
 	ezlog_init_default();
 	po::add("Help for this program")
 		("-h,help", po::NoToken, "show this message")
-		("--version,v", "show the library version")
 		.add("Author info")
 			("-name", "Luke", "the name of author")
 			("--age", 24, "the age of author")
 			("-w,weight", 66.6, "the weight of author")
 			("--height,-H", 175.6, "the height of author")
+			.add("Other infomation")
+				("girl", "DLF", "first girl friend")
+			.parent()
+		.parent()
+		
+		("--version,v", "show the library version")
 	;
 	po::parse(argc, argv);
 	if (po::get("h"))
 		po::help();
 	if (po::get("v"))
 		printf("Version 1.0.0\n");
-	printf("name: %s\nage: %d\n", po::get("name").to<std::string>().c_str(), po::get("age").to<int>());
+	printf("name: %s\n", po::get("name").to<std::string>().c_str());
+	cout << "age: " << po::get("age") << endl; //AnyBasic::operator<<()
 	float w = po::get("w");
 	double H(po::get("H"));
 	cout << "weight: " << w << endl;
 	cout << "height: " << H << endl;
+	cout << "girl: " << po::get("girl") << endl;
 	return 0;
 }
